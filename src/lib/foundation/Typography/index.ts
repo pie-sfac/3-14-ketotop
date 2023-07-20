b0755d86318071f94adcc7370f8ba1688a87b841
Sungjae.Kwak <sungjae8970@gmail.com>
2023-07-20 17:02:01 +0900

    feat: font-family 시스템 추가 (#1)
    - 언어별로 다른 폰트 렌더링
    - 폰트 파일들 추가
    - 불필요한 fontFamily.ts 파일은 삭제
    - 전역적으로 폰트 css가 적용될 예정

--- src/lib/foundation/Typography/index.ts
+++ src/lib/foundation/Typography/index.ts
@@ -1,9 +1,3 @@
-import * as TypoStyleExports from './Style/typoStyle';
-import * as FontFamilyExports from './FontFamily/fontFamily';
+import * as Typography from './Style/typoStyle';
 
-const CombinedExports = {
-  typoStyle: TypoStyleExports,
-  fontFamily: FontFamilyExports,
-};
-
-export default CombinedExports;
+export { Typography };
