080310ca61d19ddfff45e44d9d6f8c99397de027
10_2pang <57481378+yedol1@users.noreply.github.com>
2023-07-20 17:50:36 +0900

    refactor: 가독성을 위한 JS Doc 추가

--- src/lib/foundation/index.ts
+++ src/lib/foundation/index.ts
@@ -4,9 +4,13 @@ import './Typography/FontFamily/font.css';
 import Layout from './Layout';
 import { Typography } from './Typography';
 
+/** Foundation 글로벌 스타일 */
 export const FoundationStyles = createGlobalStyle`
   ${ColorStyles}
 `;
 
+/** Layout 스타일 ( props : size , system ) */
 export const LayoutStyles = Layout;
+
+/** Typography 스타일 */
 export const TypographyStyles = Typography;
