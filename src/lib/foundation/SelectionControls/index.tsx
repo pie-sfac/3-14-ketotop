45cf58fa874a32e56e11ba657a9d01636788700a
TaekyungAn <atk0426@naver.com>
2023-07-21 19:09:22 +0900

    feat: input태그 이용 컴포넌트 개발

--- src/lib/foundation/SelectionControls/index.tsx
+++ src/lib/foundation/SelectionControls/index.tsx
@@ -1,43 +1,7 @@
-import { useEffect, useState } from 'react';
 import { ISelection } from './type';
 
-const SelectionControls = ({ type, selected = false, disabled = false }: ISelection) => {
-  const [selection, setSelection] = useState('');
-  const [control, setControl] = useState(true);
-  const SELECTED = selected;
-  const DISABLED = disabled;
-
-  useEffect(() => {
-    if (type === 'checkbox') {
-      if (SELECTED) {
-        if (DISABLED) setSelection(`checkbox_selected_disabled`);
-        else setSelection('checkbox_selected_enabled');
-      } else {
-        if (DISABLED) setSelection('checkbox_unselected_disabled');
-        else setSelection('checkbox_unselected_enabled');
-      }
-    } else if (type === 'radio') {
-      if (SELECTED) {
-        if (DISABLED) setSelection('radio_selected_disabled');
-        else setSelection('radio_selected_enabled');
-      } else {
-        if (DISABLED) setSelection('radio_unselected_disabled');
-        else setSelection('radio_unselected_enabled');
-      }
-    }
-  }, []);
-
-  console.log(selection);
-  const onClick = (e: React.MouseEvent) => {
-    console.log(e);
-    selected = true;
-  };
-
-  return (
-    <div onClick={onClick}>
-      <img src={`../public/assets/Icons/selection/${selection}.svg`} alt={type} />
-    </div>
-  );
+const SelectionControls = ({ type, selected, disabled }: ISelection) => {
+  return <input style={{ accentColor: '#618dfe' }} type={type} checked={selected} disabled={disabled} />;
 };
 
 export default SelectionControls;
