7cddda6b605ca828cde9eda652c0691fba126c56
TaekyungAn <atk0426@naver.com>
2023-07-21 15:29:35 +0900

    feat: 아이콘 컴포넌트 개발 및 타입지정

--- src/lib/foundation/Icon/index.tsx
+++ src/lib/foundation/Icon/index.tsx
@@ -1,5 +1,13 @@
-const Icon = () => {
-  return null;
+import { IIcon } from './type';
+
+const Icon = ({ name, height, width }: IIcon) => {
+  return (
+    <>
+      <img src={`../public/assets/Icon/${name}.svg`} alt={name} height={height} width={width} />
+    </>
+  );
 };
-// <Icon name={notice} size={} />
 export default Icon;
+
+// 레포지토리 src 내 assets폴더 이용시 url
+// https://raw.githubusercontent.com/3-14-ketotop/3-14-ketotop/dev/src/assets/Icons/Add.svg
