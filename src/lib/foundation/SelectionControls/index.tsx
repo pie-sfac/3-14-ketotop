15f08b8714e5617d5120c1da70bf0776fbc34482
TaekyungAn <atk0426@naver.com>
2023-07-21 19:32:47 +0900

    style: 지정 color 사용

--- src/lib/foundation/SelectionControls/index.tsx
+++ src/lib/foundation/SelectionControls/index.tsx
@@ -1,7 +1,11 @@
+import { styled } from 'styled-components';
 import { ISelection } from './type';
 
+const SelectionInput = styled.input`
+  accent-color: var(--Pri_400);
+`;
 const SelectionControls = ({ type, selected, disabled }: ISelection) => {
-  return <input style={{ accentColor: '#618dfe' }} type={type} checked={selected} disabled={disabled} />;
+  return <SelectionInput type={type} checked={selected} disabled={disabled} />;
 };
 
 export default SelectionControls;
