03c4fa4bc81748116cdf2cfa6ceff51561366f7e
TaekyungAn <atk0426@naver.com>
2023-07-23 16:15:21 +0900

    fix: checked prop 경고 해결

--- src/lib/foundation/SelectionControls/index.tsx
+++ src/lib/foundation/SelectionControls/index.tsx
@@ -5,7 +5,7 @@ const SelectionInput = styled.input`
   accent-color: var(--Pri_400);
 `;
 const SelectionControls = ({ type, selected, disabled }: ISelection) => {
-  return <SelectionInput type={type} checked={selected} disabled={disabled} />;
+  return <SelectionInput type={type} defaultChecked={selected} disabled={disabled} />;
 };
 
 export default SelectionControls;
