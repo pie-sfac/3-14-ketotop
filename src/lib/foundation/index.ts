3f828591e24d33e85d41eb07f037391db77f2b94
10_2pang <57481378+yedol1@users.noreply.github.com>
2023-07-24 14:11:42 +0900

    refactor: selection 스타일을 모듈에 추가

--- src/lib/foundation/index.ts
+++ src/lib/foundation/index.ts
@@ -4,6 +4,7 @@ import './Typography/FontFamily/font.css';
 import Layout from './Layout';
 import { Typography } from './Typography';
 import Icon from '@lib/foundation/Icon';
+import SelectionControls from '@lib/foundation/SelectionControls';
 
 /** Foundation 글로벌 스타일 */
 const FoundationStyles = createGlobalStyle`
@@ -19,5 +20,7 @@ const TypographyStyles = Typography;
 /** Icon 스타일 ( props : IconName , height , width )*/
 const IconStyles = Icon;
 
+const SelectionControlsStyles = SelectionControls;
+
 // 모든 스타일 export
-export { FoundationStyles, LayoutStyles, TypographyStyles, IconStyles };
+export { FoundationStyles, LayoutStyles, TypographyStyles, IconStyles, SelectionControlsStyles };
