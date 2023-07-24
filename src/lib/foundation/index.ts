626a31e2c54e7e95c49cab3129343f2c7a20851d
10_2pang <57481378+yedol1@users.noreply.github.com>
2023-07-24 14:00:56 +0900

    refactor: 현재까지 파운데이션 파트 모듈화 작업

--- src/lib/foundation/index.ts
+++ src/lib/foundation/index.ts
@@ -3,14 +3,21 @@ import { ColorStyles } from './Color';
 import './Typography/FontFamily/font.css';
 import Layout from './Layout';
 import { Typography } from './Typography';
+import Icon from '@lib/foundation/Icon';
 
 /** Foundation 글로벌 스타일 */
-export const FoundationStyles = createGlobalStyle`
+const FoundationStyles = createGlobalStyle`
   ${ColorStyles}
 `;
 
 /** Layout 스타일 ( props : size , system ) */
-export const LayoutStyles = Layout;
+const LayoutStyles = Layout;
 
 /** Typography 스타일 */
-export const TypographyStyles = Typography;
+const TypographyStyles = Typography;
+
+/** Icon 스타일 ( props : IconName , height , width )*/
+const IconStyles = Icon;
+
+// 모든 스타일 export
+export { FoundationStyles, LayoutStyles, TypographyStyles, IconStyles };
