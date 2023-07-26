d4314774ebbe3cae800c1bebd9150998413dac8d
10_2pang <57481378+yedol1@users.noreply.github.com>
2023-07-26 12:29:17 +0900

    refactor: 모듈 JSDoc 수정 및 export 추가

--- src/lib/foundation/index.ts
+++ src/lib/foundation/index.ts
@@ -6,6 +6,7 @@ import Layout from './Layout';
 import { Typography } from './Typography';
 import Icon from '@lib/foundation/Icon';
 import SelectionControls from '@lib/foundation/SelectionControls';
+import Graphic from './Graphic';
 
 /** Foundation 글로벌 스타일 */
 const FoundationStyles = createGlobalStyle`
@@ -13,17 +14,20 @@ const FoundationStyles = createGlobalStyle`
   ${ColorStyles}
 `;
 
-/** Layout 스타일 ( props : size , system ) */
+/** Layout 스타일 ( props : children , size (tablet) , system (android) ) */
 const LayoutStyles = Layout;
 
 /** Typography 스타일 */
 const TypographyStyles = Typography;
 
-/** Icon 스타일 ( props : IconName , height , width )*/
+/** Icon 스타일 ( props : name (필수) , extension (svg) , height , width )*/
 const IconStyles = Icon;
 
-/** SelectionControls 스타일 ( props : type , selected , disabled ) */
+/** Graphic 스타일 ( props : name (필수) , extension (svg) , height , width , category )*/
+const GraphicStyles = Graphic;
+
+/** SelectionControls 스타일 ( props : type (필수) , selected , disabled ) */
 const SelectionControlsStyles = SelectionControls;
 
 // 모든 스타일 export
-export { FoundationStyles, LayoutStyles, TypographyStyles, IconStyles, SelectionControlsStyles };
+export { FoundationStyles, LayoutStyles, TypographyStyles, IconStyles, GraphicStyles, SelectionControlsStyles };
