583ba73719be962a0a5a9d5c11718664e8862093
10_2pang <57481378+yedol1@users.noreply.github.com>
2023-07-27 16:37:27 +0900

    refactor: Graphic, Icon 의 name 프로퍼티 자동완성 가능하도록 리팩토링

--- src/lib/foundation/Icon/type.ts
+++ src/lib/foundation/Icon/type.ts
@@ -1,6 +1,69 @@
-export interface IIcon {
-  name: string;
-  extension: string;
+interface IIconStrict {
+  name: IconName;
+  extension?: string;
   height?: number;
   width?: number;
 }
+
+interface IIconGeneral {
+  name: string;
+  extension?: string;
+  height?: number;
+  width?: number;
+}
+
+export type IIcon = IIconStrict | IIconGeneral;
+
+type IconName =
+  | 'notice'
+  | 'search'
+  | 'close'
+  | 'back'
+  | 'drop_more'
+  | 'drop_less'
+  | 'add'
+  | 'deletecircle_gray'
+  | 'delete'
+  | 'more_vert'
+  | 'arrow_more'
+  | 'visibility_on'
+  | 'visibility_off'
+  | 'half_star'
+  | 'star_on'
+  | 'star_off'
+  | 'comment'
+  | 'videoform'
+  | 'edit'
+  | 'message'
+  | 'document'
+  | 'undo'
+  | 'redo'
+  | 'reset'
+  | 'delete_small'
+  | 'time'
+  | 'back_small'
+  | 'next_small'
+  | 'more_small'
+  | 'less_small'
+  | 'calendar'
+  | 'setting'
+  | 'filter'
+  | 'centerinfo'
+  | 'help'
+  | 'up'
+  | 'down'
+  | 'bin'
+  | 'thumbnails_checkcircle'
+  | 'thumbnails_deletecircle'
+  | 'thumbnails_playcircle'
+  | 'thumbnails_errorcircle'
+  | 'appbar_center'
+  | 'appbar_center_gray'
+  | 'appbar_home'
+  | 'appbar_home_gray'
+  | 'appbar_mypage'
+  | 'appbar_mypage_gray'
+  | 'appbar_patient'
+  | 'appbar_patient_gray'
+  | 'appbar_reserve'
+  | 'appbar_reserve_gray';
--- src/lib/foundation/index.ts
+++ src/lib/foundation/index.ts
@@ -8,7 +8,7 @@ import SelectionControls from './SelectionControls';
 import Graphic from './Graphic';
 
 /** Foundation 글로벌 스타일 */
-const FoundationStyles = createGlobalStyle`
+const FoundationGlobalStyles = createGlobalStyle`
   ${ResetCSS}
   ${ColorStyles}
   ${FontFamilyStyle}
@@ -20,14 +20,14 @@ const LayoutStyles = Layout;
 /** Typography 스타일 */
 const TypographyStyles = Typography;
 
-/** Icon 스타일 ( props : name (필수) , extension (svg) , height , width )*/
+/** Icon 스타일 ( props : name (필수 , 자동완성 사용시 name={''} 형태로 사용해야함) , extension (svg) , height , width ) */
 const IconStyles = Icon;
 
-/** Graphic 스타일 ( props : name (필수) , extension (svg) , height , width , category )*/
+/** Graphic 스타일 ( props : name (필수 , 자동완성 사용시 name={''} 형태로 사용해야함) , extension (svg) , height , width , category )*/
 const GraphicStyles = Graphic;
 
 /** SelectionControls 스타일 ( props : type (필수) , selected , disabled ) */
 const SelectionControlsStyles = SelectionControls;
 
 // 모든 스타일 export
-export { FoundationStyles, LayoutStyles, TypographyStyles, IconStyles, GraphicStyles, SelectionControlsStyles };
+export { FoundationGlobalStyles, LayoutStyles, TypographyStyles, IconStyles, GraphicStyles, SelectionControlsStyles };
