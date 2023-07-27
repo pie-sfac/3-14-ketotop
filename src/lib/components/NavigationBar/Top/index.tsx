cc658e133a5a30e1d40cfe38d7af0525eb44ae41
TaekyungAn <atk0426@naver.com>
2023-07-27 18:18:48 +0900

    style: 네비top 스타일 적용 완료

--- src/lib/components/NavigationBar/Top/index.tsx
+++ src/lib/components/NavigationBar/Top/index.tsx
@@ -2,33 +2,52 @@ import { GraphicStyles, TypographyStyles } from '@lib/foundation';
 import * as St from './style';
 import { INavTop } from './type';
 import Icon from '@lib/foundation/Icon';
+import { useState } from 'react';
 
 const NavigationBarTop = ({ page = 'home' }: INavTop) => {
   const centerList = ['직원 관리', '수강권 관리', '기록 관리', '미디어 관리', '운영 데이터', '알림메시지', '센터 정보'];
   const mypage = ['내 정보', '공지사항', '앱 설정', '운영데이터'];
   const memberName = '박관리자01';
+
+  const [currentTab, setCurrentTab] = useState(0);
+
   return (
     <St.NavTopContainer>
       <GraphicStyles name='logo' extension='svg' />
 
       {/* 타이포그래피 바디3 세미볼드 적용시켜야 함 */}
-      <St.ItemsList>
-        {page === 'center' && centerList.map((item) => <li>{item}</li>)}
-        {page === 'mypage' && mypage.map((item) => <li>{item}</li>)}
-      </St.ItemsList>
+      <TypographyStyles.Body3 as={'div'}>
+        <St.ItemsList>
+          {page === 'center' &&
+            centerList.map((item, index) => (
+              <li key={item} onClick={() => setCurrentTab(index)} className={currentTab === index ? ' focused' : ''}>
+                {item}
+              </li>
+            ))}
+          {page === 'mypage' &&
+            mypage.map((item, index) => (
+              <li key={item} onClick={() => setCurrentTab(index)} className={currentTab === index ? ' focused' : ''}>
+                {item}
+              </li>
+            ))}
+        </St.ItemsList>
+      </TypographyStyles.Body3>
 
-      <St.FixedItems>
-        <St.Line100 />
-        <St.UserInfo>
-          <GraphicStyles name='Profile_24px' extension='svg' />
-          <span>{memberName}</span>
-          <div>플랜 이용중</div>
-        </St.UserInfo>
-        <St.Line100 />
-        <St.IconWrapper>
-          <Icon name='notice' extension='svg' />
-        </St.IconWrapper>
-      </St.FixedItems>
+      <TypographyStyles.Body4 as={'div'}>
+        <St.FixedItems>
+          <St.Line100 />
+          <St.UserInfo>
+            <GraphicStyles name='Profile_24px' extension='svg' />
+            <span>{memberName}</span>
+            <div>플랜 이용중</div>
+          </St.UserInfo>
+          <St.Line100 />
+
+          <St.IconWrapper>
+            <Icon name='notice' extension='svg' />
+          </St.IconWrapper>
+        </St.FixedItems>
+      </TypographyStyles.Body4>
     </St.NavTopContainer>
   );
 };
--- src/lib/components/NavigationBar/Top/style.ts
+++ src/lib/components/NavigationBar/Top/style.ts
@@ -10,33 +10,44 @@ export const NavTopContainer = styled.div`
   padding: 0 24px;
 
   color: var(--Text_900);
-  // 임시설정
-  box-sizing: border-box;
+
   background-color: white;
+  box-sizing: border-box;
+  // 임시설정
   margin-bottom: 10px;
 
   font-size: 14px;
   font-weight: 500;
 `;
 
+// 로고 + 메뉴
+export const FixedItems = styled.div`
+  display: flex;
+
+  position: absolute;
+  right: 32px;
+  top: 50%;
+  transform: translate(0, -50%);
+`;
+
+// 메뉴 리스트
 export const ItemsList = styled.ul`
   display: flex;
   li {
     margin-left: 32px;
     cursor: pointer;
+    &.focused {
+      color: var(--Pri_500);
+    }
   }
 `;
 
-export const FixedItems = styled.div`
-  display: flex;
-  position: absolute;
-  right: 32px;
-`;
 export const UserInfo = styled.div`
   display: flex;
   align-items: center;
   margin: 0 16px;
 
+  // 구독상태
   div {
     width: 62px;
     height: 21px;
@@ -50,18 +61,22 @@ export const UserInfo = styled.div`
     color: var(--Pri_500);
     background-color: var(--Bg_100);
   }
+
+  // 사용자 이름
   span {
     display: inline-block;
     margin: 0 8px;
   }
 `;
 
+// 분류 선
 export const Line100 = styled.div`
   height: 22px;
   width: 1px;
   background-color: var(--Line_200);
 `;
 
+// 아이콘 마진 설정
 export const IconWrapper = styled.div`
   margin-left: 12px;
 `;
