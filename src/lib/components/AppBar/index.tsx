efe3417504f41e32082a5801636f4a02fe8c9a0f
TaekyungAn <atk0426@naver.com>
2023-07-31 22:29:17 +0900

    add: appbar 기능 구현 완료 파일 추가

---
+++ src/lib/components/AppBar/index.tsx
@@ -0,0 +1,70 @@
+import { IconStyles } from '@lib/foundation';
+import {
+  AppBarContainer,
+  DefalutLayout,
+  FixedItems,
+  FullPopupLayout,
+  IconBox,
+  IconBoxNomargin,
+  IconItems,
+  TextBox,
+} from './styles';
+import { IAppBar } from './type';
+
+const AppBar = ({ pagename = '페이지네임', size, type }: IAppBar) => {
+  return (
+    <AppBarContainer size={size}>
+      {
+        // full (페이지네임 + X 아이콘)
+        size === 'full' ? (
+          <FullPopupLayout>
+            <div>{pagename}</div>
+            <IconBoxNomargin>
+              <IconStyles name={'delete'} />
+            </IconBoxNomargin>
+          </FullPopupLayout>
+        ) : (
+          // large or medium (<뒤로가기아이콘 + 페이지네임 + 아이콘||텍스트)
+          <DefalutLayout>
+            <FixedItems>
+              <IconBox>
+                <IconStyles name={`back`} />
+              </IconBox>
+              <div>{pagename}</div>
+            </FixedItems>
+
+            {type?.icon && (
+              <IconItems>
+                {type?.icon.icon_L && (
+                  <IconBox>
+                    <IconStyles name={`${type.icon?.icon_L}`} />
+                  </IconBox>
+                )}
+                {type.icon.icon_R && (
+                  <IconBoxNomargin>
+                    <IconStyles name={`${type.icon?.icon_R}`} />
+                  </IconBoxNomargin>
+                )}
+              </IconItems>
+            )}
+
+            {type?.text && (
+              <IconItems>
+                {type?.text.text_L && <TextBox>{type?.text?.text_L}</TextBox>}
+                {type?.text.text_R && <TextBox>{type?.text?.text_R}</TextBox>}
+              </IconItems>
+            )}
+
+            {type?.count && (
+              <TextBox>
+                {type?.count.text}({type?.count.count})
+              </TextBox>
+            )}
+          </DefalutLayout>
+        )
+      }
+    </AppBarContainer>
+  );
+};
+
+export default AppBar;
---
+++ src/lib/components/AppBar/styles.ts
@@ -0,0 +1,82 @@
+import { styled } from 'styled-components';
+import { IAppBar } from './type';
+
+export const AppBarContainer = styled.div<IAppBar>`
+  width: ${(props) => (props.size === 'medium' ? '650px' : '1024px')};
+  height: ${(props) => (props.size === 'full' ? '56px' : '48px')};
+  box-sizing: border-box;
+
+  background-color: gray;
+
+  //임시스타일
+  margin-bottom: 10px;
+`;
+
+// 중간: 페이지네임 우:닫힘 의 레이아웃 (size: full)
+export const FullPopupLayout = styled.div<IAppBar>`
+  // 이게 안들어가면 오차가 생김... 추후에 다시 보기
+  height: 56px;
+  box-sizing: border-box;
+  //
+
+  padding: 16px 24px;
+  position: relative;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  :last-child {
+    position: absolute;
+    right: 24px;
+    transform: translate(50%, 0);
+  }
+`;
+
+// 좌:뒤로가기 우:버튼 의 레이아웃 (size: large, medium)
+export const DefalutLayout = styled.div<IAppBar>`
+  padding: 12px 16px;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+// 아이콘 감싸는 박스
+export const IconBox = styled.div`
+  height: 24px;
+  width: 24px;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  margin-right: 8px;
+
+  img {
+    cursor: pointer;
+  }
+`;
+
+// 아이콘 감싸는 박스 마진 없는 버전
+export const IconBoxNomargin = styled(IconBox)`
+  margin-right: 0px;
+`;
+
+// 텍스트 감싸는 박스
+export const TextBox = styled.div`
+  margin-left: 20px;
+  cursor: pointer;
+`;
+
+// 좌측 아이템 (뒤로가기아이콘, 페이지네임)
+export const FixedItems = styled.div`
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+`;
+
+export const IconItems = styled.div`
+  display: flex;
+  align-items: center;
+`;
