eff5848e14fea23236b38946d2fbd5ffce634840
10_2pang <57481378+yedol1@users.noreply.github.com>
2023-07-31 20:12:52 +0900

    add: Item 컴포넌트 파일 디렉토리 구조 추가

---
+++ src/lib/components/Item/MemberOne/index.tsx
@@ -0,0 +1,8 @@
+import ItemLayout from '@lib/components/Item/styles';
+import { Fnd } from '@src/lib';
+
+const MemberOne = () => {
+  return <ItemLayout>MemberOne</ItemLayout>;
+};
+
+export default MemberOne;
---
+++ src/lib/components/Item/styles.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+const ItemLayout = styled.section`
+  display: flex;
+  width: 976px;
+  height: 48px;
+  padding: 10px 24px;
+  justify-content: center;
+  align-items: center;
+  flex-shrink: 0;
+
+  border-radius: 4px;
+  //백그라운드는 프롭스에 따라 수정해야함
+  background-color: #ffffff;
+`;
+export default ItemLayout;
