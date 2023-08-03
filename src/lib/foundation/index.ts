ec607b42cb3fe30a94f17e1e41db8cad8d218600
10_2pang <57481378+yedol1@users.noreply.github.com>
2023-08-03 14:54:13 +0900

    refactor: JSDoc 컨벤션에 맞게 수정

--- src/lib/foundation/index.ts
+++ src/lib/foundation/index.ts
@@ -8,29 +8,40 @@ import SelectionControls from './SelectionControls';
 import Graphic from './Graphic';
 import ShadowBox from './Shadow/styled';
 
-/** Foundation 글로벌 스타일 */
+/** @description Foundation 글로벌 스타일 */
 const FoundationGlobalStyles = createGlobalStyle`
   ${ResetCSS}
   ${FontFamilyStyle}
   ${ColorStyles}
 `;
 
-/** Layout 스타일 ( props : children , size (tablet) , system (android) ) */
+/**
+ * @description Layout 스타일
+ * @type {(children : ReactNode) , (size : 'android' | 'ios' ( 기본:tablet )) , (system : 'tablet' | 'pc' | 'phone' ( 기본:android ))} ) */
 const LayoutStyles = Layout;
 
-/** Typography 스타일 */
+/** @description Typography 스타일 */
 const TypographyStyles = Typography;
 
-/** Icon 스타일 ( props : name (필수 , 자동완성 사용시 name={''} 형태로 사용해야함) , extension (svg) , height , width ) */
+/**
+ * @description Icon 스타일
+ * @type {( name : string (필수) , 자동완성 사용시 name={''} 형태로 사용해야함) , (extension : string ( 기본:svg )) , (height : number) , (width : number)}
+ */
 const IconStyles = Icon;
 
-/** Graphic 스타일 ( props : name (필수 , 자동완성 사용시 name={''} 형태로 사용해야함) , extension (svg) , height , width , category )*/
+/**
+ * @description Graphic 스타일
+ * @type {( name : string (필수) , 자동완성 사용시 name={''} 형태로 사용해야함) , (extension : string ( 기본:svg )) , (height : number) , (width : number)}
+ */
 const GraphicStyles = Graphic;
 
-/** SelectionControls 스타일 ( props : type (필수) , selected , disabled ) */
+/**
+ * @description SelectionControls 스타일
+ * @type {( type : 'checkbox' | 'radio' (필수) , selected : boolean , disabled : boolean)} */
 const SelectionControlsStyles = SelectionControls;
 
-/** ShadowBoxStyles 스타일 ( css 파일에 ${Fnd.ShadowBoxStyles} 를 넣으면 적용가능 ) */
+/**
+ * @description ShadowBoxStyles 스타일 ( css 파일에 ${Fnd.ShadowBoxStyles} 를 넣으면 적용가능 ) */
 const ShadowBoxStyles = ShadowBox;
 
 // 모든 스타일 export
