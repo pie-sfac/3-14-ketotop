6b22644fb66ddaf345e0e83dd9c9104e65bc00e2
chasura416 <chasura416@naver.com>
2023-08-04 23:53:59 +0900

    add: FAB 속성값 추가

---
+++ src/lib/components/ButtonFAB/index.tsx
@@ -0,0 +1,18 @@
+import * as St from './styles';
+import { Fnd } from '../..';
+import { Cmp } from '../..';
+
+const FloatingActionButtons = () => {
+  return(
+    <>
+    <Fnd.FoundationGlobalStyles />
+    <St.StFAB>
+      <Cmp.Button.Contained size={''} state={''}>
+        
+      </Cmp.Button.Contained>
+    </St.StFAB>
+    </>
+  );
+}
+
+export default FloatingActionButtons;
---
+++ src/lib/components/ButtonFAB/styles.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+import { Fab } from "@mui/material";
+
+
+export const StFAB = styled.(Fab)`
+  position: fixed;
+  bottom: 32px;
+  right: 32px;
+`;
