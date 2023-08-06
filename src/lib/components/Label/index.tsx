06c461cfe05444aef0dc6fd241949b859cb2b262
10_2pang <57481378+yedol1@users.noreply.github.com>
2023-08-06 16:10:42 +0900

    style: 경로 수정

--- src/lib/components/Label/index.tsx
+++ src/lib/components/Label/index.tsx
@@ -1,24 +1,23 @@
-import { Fnd } from '@lib/index';
-import {LabelType} from './type';
+import { LabelType } from './type';
 import * as St from './styles';
+import { Fnd } from '../..';
 
-const Label = ({ type,bgcolor,fontcolor, children }: LabelType) => {
+const Label = ({ type, bgcolor, fontcolor, children }: LabelType) => {
   let TypographyComponent;
-
   switch (type) {
     case 'Rectangle':
       TypographyComponent = Fnd.TypographyStyles.Caption2;
       break;
-      case 'Round':
+    case 'Round':
       TypographyComponent = Fnd.TypographyStyles.Caption1;
       break;
   }
   return (
     <>
       <Fnd.FoundationGlobalStyles />
-        <St.LabelWrapper $typename={type} $bgcolor={bgcolor} $fontcolor={fontcolor}>
-          <TypographyComponent>{children}</TypographyComponent>
-        </St.LabelWrapper>
+      <St.LabelWrapper $typename={type} $bgcolor={bgcolor} $fontcolor={fontcolor}>
+        <TypographyComponent>{children}</TypographyComponent>
+      </St.LabelWrapper>
     </>
   );
 };
