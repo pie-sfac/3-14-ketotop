893d37dd06dcec5da9cce4a0e3c443c4c1b4e517
10_2pang <57481378+yedol1@users.noreply.github.com>
2023-08-06 16:11:18 +0900

    refactor: css 관련 부분 수정 및 불필요한 코드 삭제

--- src/lib/components/Label/styles.ts
+++ src/lib/components/Label/styles.ts
@@ -1,69 +1,67 @@
-import { styled } from "styled-components";
-import { TypeName, BgColor, FontColor } from "./type";
+import { styled } from 'styled-components';
+import { TypeName, BgColor, FontColor } from './type';
 
-export const LabelShape = ({$typename}:{ $typename: TypeName}) => {
-  switch($typename) {
-    case "Rectangle":
-      return`
-        font-family: var(Pretendard);
+export const LabelShape = ({ $typename }: { $typename: TypeName }) => {
+  switch ($typename) {
+    case 'Rectangle':
+      return `
         display: inline-flex;
         padding: 4px 8px;
         justify-content: center;
         align-items: center;
         gap: 10px;
         border-radius: 4px;
-      `
-    case "Round":
-      return`
-        font-family: var(Roboto);
+      `;
+    case 'Round':
+      return `
         display: inline-flex;
         padding: 4px 8px;
         justify-content: center;
         align-items: center;
         gap: 10px;
         border-radius: 100px;
-      `
+      `;
   }
-}
+};
 
-export const LabelBgColor = ({$bgcolor}:{ $bgcolor: BgColor}) => {
+export const LabelBgColor = ({ $bgcolor }: { $bgcolor: BgColor }) => {
   switch ($bgcolor) {
     case 'Blue':
-      return`
+      return `
         background-color: var(--Bgc_50);
-      `
-      case 'Gray':
-      return`
+      `;
+    case 'Gray':
+      return `
         background-color: var(--Bg_100);
-        `
-      case 'Green':
-      return`
+        `;
+    case 'Green':
+      return `
         background-color: #E6F9EA;
-        `
+        `;
   }
-}
-export const LabelFontColor = ({$fontcolor}: {$fontcolor: FontColor}) => {
+};
+export const LabelFontColor = ({ $fontcolor }: { $fontcolor: FontColor }) => {
   switch ($fontcolor) {
     case 'Blue':
-      return`
-        background-color: var(--Pri_500);
-      `
-      case 'Gray':
-      return`
-        background-color: var(--Text_700);
-        `
-      case 'Green':
-      return`
-        background-color: var(--System_Positive);
-        `
-      case 'Red':
-      return`
-        background-color: var(--System_Error);
-      `
+      return `
+        color: var(--Pri_500);
+      `;
+    case 'Gray':
+      return `
+        color: var(--Text_700);
+        `;
+    case 'Green':
+      return `
+        color: var(--System_Positive);
+        `;
+    case 'Red':
+      return `
+        color: var(--System_Error);
+      `;
   }
-}
+};
 
-export const LabelWrapper = styled.label<{$typename: TypeName; $bgcolor: BgColor; $fontcolor: FontColor; }>`
+export const LabelWrapper = styled.label<{ $typename: TypeName; $bgcolor: BgColor; $fontcolor: FontColor }>`
   ${LabelShape}
   ${LabelBgColor}
   ${LabelFontColor}
