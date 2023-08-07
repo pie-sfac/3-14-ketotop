120ccb9bd58ba3c520c1da7170db9d3618264431
TaekyungAn <atk0426@naver.com>
2023-08-07 13:33:29 +0900

    style: 스타일 속성 변경

--- src/lib/foundation/Icon/styles.ts
+++ src/lib/foundation/Icon/styles.ts
@@ -1,7 +1,7 @@
 import { styled } from 'styled-components';
 
-export const IconContainer = styled.span<{ height: number; width: number; iconSize: number }>`
-  display: flex;
+export const IconContainer = styled.div<{ height: number; width: number; iconSize: number }>`
+  display: inline-flex;
   justify-content: center;
   align-items: center;
 
