4d5381f4566f54ecd898748ba5cfad1abbde4c94
TaekyungAn <atk0426@naver.com>
2023-08-07 20:25:28 +0900

    bug: JS환경 에러핸들링 추가

--- src/lib/foundation/SelectionControls/Input/index.tsx
+++ src/lib/foundation/SelectionControls/Input/index.tsx
@@ -2,6 +2,12 @@ import * as St from '../styles';
 import { IInput } from '../type';
 
 const SelectionInput = ({ type, disabled, checked, id, ...otherProps }: IInput) => {
+  if (type !== 'checkbox' && type !== 'radio') throw new Error('type name Error. Check your spelling again');
+  if (disabled && disabled !== true && disabled !== false)
+    throw new Error('disabled prop should be true or false. Check your spelling again');
+  if (checked && checked !== true && checked !== false)
+    throw new Error('checked prop should be true or false. Check your spelling again');
+
   return (
     <St.SInputBox>
       <St.SInput type={type} disabled={disabled} checked={checked} id={id} {...otherProps} readOnly />
