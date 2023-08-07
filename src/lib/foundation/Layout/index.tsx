e5821431c96472ce59322133f5c2b36e9a742d79
TaekyungAn <atk0426@naver.com>
2023-08-07 16:28:24 +0900

    feat: JS환경에서 props전달 에러핸들링

--- src/lib/foundation/Layout/index.tsx
+++ src/lib/foundation/Layout/index.tsx
@@ -1,7 +1,13 @@
 import { ILayout } from './type';
 import { LayoutContainer } from './styles';
+import { useEffect, useState } from 'react';
 
 const Layout = ({ children, $size = 'tablet', $system = 'android' }: ILayout) => {
+  if ($system !== "android" && $system !== "ios")
+    throw new Error("$system 값에 오류가 있습니다");
+  if ($size !== "tablet" && $size !== "pc" && $size !== "phone")
+    throw new Error("$size 값에 오류가 있습니다");
+
   return (
     <LayoutContainer $size={$size} $system={$system}>
       {children}
