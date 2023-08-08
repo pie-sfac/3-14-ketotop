05a6edfc1846c5760614a7b8f4f664f85fd8f752
TaekyungAn <atk0426@naver.com>
2023-08-08 21:13:07 +0900

    style: 타입 확장

--- src/lib/components/Thumbnails/type.ts
+++ src/lib/components/Thumbnails/type.ts
@@ -1,12 +1,12 @@
 import { HTMLAttributes, HtmlHTMLAttributes } from 'react';
 
-export interface IVideo {
+export interface IVideo extends React.HTMLAttributes<HTMLElement> {
   src?: string;
   $state?: VideoState;
   runningtime?: number;
 }
 
-export interface IImage {
+export interface IImage extends React.HTMLAttributes<HTMLElement> {
   src: string;
   $state?: ImageState;
 }
@@ -14,4 +14,6 @@ export interface IImage {
 type VideoState = 'normal' | 'delete' | 'select' | 'video_play' | 'error';
 type ImageState = 'normal' | 'delete' | 'select' | 'error';
 
-export interface IDelete extends HTMLAttributes<HTMLElement> {}
+export interface IDelete extends React.HTMLAttributes<HTMLElement> {
+  name: string;
+}
