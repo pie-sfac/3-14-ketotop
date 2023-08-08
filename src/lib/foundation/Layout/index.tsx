fcefaccdc874e85a9e5be8e25cdbd3564094e9ab
Sungjae.Kwak <sungjae8970@gmail.com>
2023-08-08 20:19:08 +0900

    chore: 안쓰는 코드 삭제

--- src/lib/foundation/Layout/index.tsx
+++ src/lib/foundation/Layout/index.tsx
@@ -1,12 +1,9 @@
 import { ILayout } from './type';
 import { LayoutContainer } from './styles';
-import { useEffect, useState } from 'react';
 
 const Layout = ({ children, $size = 'tablet', $system = 'android' }: ILayout) => {
-  if ($system !== "android" && $system !== "ios")
-    throw new Error("$system 값에 오류가 있습니다");
-  if ($size !== "tablet" && $size !== "pc" && $size !== "phone")
-    throw new Error("$size 값에 오류가 있습니다");
+  if ($system !== 'android' && $system !== 'ios') throw new Error('$system 값에 오류가 있습니다');
+  if ($size !== 'tablet' && $size !== 'pc' && $size !== 'phone') throw new Error('$size 값에 오류가 있습니다');
 
   return (
     <LayoutContainer $size={$size} $system={$system}>
