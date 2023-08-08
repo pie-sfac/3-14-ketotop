4c87c2e7aec4d61e2e14a1bab0a20d775290ba34
chasura416 <chasura416@naver.com>
2023-08-08 16:59:23 +0900

    remove: 불필요한코드 삭제

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { StyledEngineProvider } from '@mui/material/styles';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <StyledEngineProvider injectFirst>
-      <App />
-    </StyledEngineProvider>    
+    <App />
   </React.StrictMode>,
 );
