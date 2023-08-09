b391fa1d1282c65ce94b32149df46d17f844f64a
10_2pang <57481378+yedol1@users.noreply.github.com>
2023-08-09 21:03:54 +0900

    refactor: Tabs 컴포넌트 버그 수정 및 리팩토링 작업

--- src/lib/components/Tabs/TabComponent/styles.ts
+++ src/lib/components/Tabs/TabComponent/styles.ts
@@ -1,12 +1,13 @@
 import styled from 'styled-components';
 
-export const TabComponentWrapper = styled.div<{ instance: 'Select' | 'Unselect' }>`
+export const TabComponentWrapper = styled.div<{ $instance: 'Select' | 'Unselect' }>`
   display: inline-flex;
-  padding: ${(props) => (props.instance === 'Select' ? '8px 12px 6px' : '8px 12px 7px')};
+  padding: ${(props) => (props.$instance === 'Select' ? '8px 12px 6px' : '8px 12px 7px')};
   justify-content: center;
   align-items: center;
   gap: 10px;
-  color: ${(props) => (props.instance === 'Select' ? 'var(--Pri_300)' : 'var(--Line_300)')};
-  border-bottom: ${(props) => (props.instance === 'Select' ? '2px solid var(--Pri_300)' : '1px solid var(--Line_300)')};
+  color: ${(props) => (props.$instance === 'Select' ? 'var(--Pri_300)' : 'var(--Line_300)')};
+  border-bottom: ${(props) =>
+    props.$instance === 'Select' ? '2px solid var(--Pri_300)' : '1px solid var(--Line_300)'};
   cursor: pointer;
 `;
