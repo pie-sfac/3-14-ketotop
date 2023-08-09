5c03fde54e4e42ae4a432fc98fa0519cb65ca4fc
Sungjae.Kwak <sungjae8970@gmail.com>
2023-08-09 15:53:17 +0900

    refactor: label 기본 속성 따로 타입캐스팅 하지 않음

--- src/lib/foundation/SelectionControls/Label/index.tsx
+++ src/lib/foundation/SelectionControls/Label/index.tsx
@@ -1,12 +1,8 @@
 import * as St from '../styles';
 import { ILabel } from '../type';
 
-const SelectionLabel = ({ content, htmlFor, ...otherProps }: ILabel) => {
-  return (
-    <St.SLabel htmlFor={htmlFor} {...otherProps}>
-      {content}
-    </St.SLabel>
-  );
+const SelectionLabel = ({ content, ...labelAttrs }: ILabel) => {
+  return <St.SLabel {...labelAttrs}>{content}</St.SLabel>;
 };
 
 export default SelectionLabel;
