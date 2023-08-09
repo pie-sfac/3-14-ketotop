cca995b232c51676674eaa1aaaab4d7e16795799
Sungjae.Kwak <sungjae8970@gmail.com>
2023-08-09 17:42:55 +0900

    chore: 안쓰는 코드 삭제

--- src/lib/components/AppBar/index.tsx
+++ src/lib/components/AppBar/index.tsx
@@ -79,55 +79,6 @@ const AppBar = ({ pagename, size = 'large', type, children, onClick }: IAppBar) 
             </St.IconBoxNomargin>
           </St.FullPopupLayout>
         )}
-
-        {/* 아래는 원본 */}
-        {/* {
-          // full (페이지네임 + X 아이콘)
-          size === 'full' ? (
-            <St.FullPopupLayout>
-              <AppBarPagename pagename={pagename} {...pagenameAttr} />
-              <St.IconBoxNomargin>
-                <AppBarIcon name={`delete`} />
-              </St.IconBoxNomargin>
-            </St.FullPopupLayout>
-          ) : (
-            // large or medium (<뒤로가기아이콘 + 페이지네임 + 아이콘||텍스트)
-            <St.DefalutLayout>
-              <St.FixedItems>
-                <St.IconBox>
-                  <AppBarIcon name={`back`} />
-                </St.IconBox>
-                <AppBarPagename pagename={pagename} {...pagenameAttr} />
-              </St.FixedItems>
-
-              {type?.icon && (
-                <St.IconItems>
-                  {type?.icon.icon_L && (
-                    <St.IconBox>
-                      <AppBarIcon name={`${type.icon?.icon_L}`} {...iconAttr} />
-                    </St.IconBox>
-                  )}
-                  {type.icon.icon_R && (
-                    <St.IconBoxNomargin>
-                      <AppBarIcon name={`${type.icon?.icon_R}`} {...iconAttr} />
-                    </St.IconBoxNomargin>
-                  )}
-                </St.IconItems>
-              )}
-
-              {type?.text && (
-                <St.IconItems>
-                  {type?.text.text_L && <AppBarButton text={`${type?.text?.text_L}`} {...buttonAttr} />}
-                  {type?.text.text_R && <AppBarButton text={`${type?.text?.text_R}`} {...buttonAttr} />}
-                </St.IconItems>
-              )}
-
-              {type?.count && (
-                <AppBarButton text={`${type?.count.text}`} count={`${type?.count.count}`} {...buttonAttr} />
-              )}
-            </St.DefalutLayout>
-          )
-        } */}
       </St.AppBarContainer>
     </Fnd.TypographyStyles.Body1>
   );
