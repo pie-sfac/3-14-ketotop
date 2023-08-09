dbf7456c1ecc89480dffa2bc0c199be7e7bc0993
TaekyungAn <atk0426@naver.com>
2023-08-10 01:42:56 +0900

    add: BottomBar 컴포넌트분리 중간 커밋

--- src/lib/components/NavigationBar/Bottom/index.tsx
+++ src/lib/components/NavigationBar/Bottom/index.tsx
@@ -1,5 +1,6 @@
 import { IconStyles, TypographyStyles } from '../../../foundation';
 import { INavBar } from '../type';
+import BottomIcon from './Icon';
 import * as St from './style';
 
 /** @description NavigationBarBottom 스타일의 컴포넌트
@@ -11,47 +12,36 @@ const NavigationBarBottom = ({ page = 'home' }: INavBar) => {
   return (
     <St.NavBottomContainer>
       <St.MenuList>
-        <St.Menu>
-          {page === 'home' ? (
-            <IconStyles name='appbar_home' extension='svg' />
-          ) : (
-            <IconStyles name='appbar_home_gray' extension='svg' />
-          )}
+        <BottomIcon></BottomIcon>
+        {/* <St.Menu>
+          {page === 'home' ? <IconStyles name='appbar_home_24px' /> : <IconStyles name='appbar_home_gray_24px' />}
           <TypographyStyles.Caption2>홈</TypographyStyles.Caption2>
         </St.Menu>
         <St.Menu>
           {page === 'reserve' ? (
-            <IconStyles name='appbar_reserve' extension='svg' />
+            <IconStyles name='appbar_reserve_24px' />
           ) : (
-            <IconStyles name='appbar_reserve_gray' extension='svg' />
+            <IconStyles name='appbar_reserve_gray_24px' />
           )}
           <TypographyStyles.Caption2>일정관리</TypographyStyles.Caption2>
         </St.Menu>
         <St.Menu>
           {page === 'member' ? (
-            <IconStyles name='appbar_patient' extension='svg' />
+            <IconStyles name='appbar_patient_24px' />
           ) : (
-            <IconStyles name='appbar_patient_gray' extension='svg' />
+            <IconStyles name='appbar_patient_gray_24px' />
           )}
 
           <TypographyStyles.Caption2>회원관리</TypographyStyles.Caption2>
         </St.Menu>
         <St.Menu>
-          {page === 'center' ? (
-            <IconStyles name='appbar_center' extension='svg' />
-          ) : (
-            <IconStyles name='appbar_center_gray' extension='svg' />
-          )}
+          {page === 'center' ? <IconStyles name='appbar_center_24px' /> : <IconStyles name='appbar_center_gray_24px' />}
           <TypographyStyles.Caption2>센터관리</TypographyStyles.Caption2>
         </St.Menu>
         <St.Menu>
-          {page === 'mypage' ? (
-            <IconStyles name='appbar_mypage' extension='svg' />
-          ) : (
-            <IconStyles name='appbar_mypage_gray' extension='svg' />
-          )}
+          {page === 'mypage' ? <IconStyles name='appbar_mypage_24px' /> : <IconStyles name='appbar_mypage_gray_24px' />}
           <TypographyStyles.Caption2>마이페이지</TypographyStyles.Caption2>
-        </St.Menu>
+        </St.Menu> */}
       </St.MenuList>
     </St.NavBottomContainer>
   );
