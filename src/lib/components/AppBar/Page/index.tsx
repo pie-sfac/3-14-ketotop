1bc3e4775ec8f646d31bba989425c509dd4f8ca7
TaekyungAn <atk0426@naver.com>
2023-08-09 17:41:05 +0900

    remove: 불필요한 파일 제거

--- src/lib/components/AppBar/Page/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { PagenameProps } from '../type';
-
-const AppBarPagename = ({ pagename, ...otherProps }: PagenameProps) => {
-  return <div {...otherProps}>{pagename}</div>;
-};
-
-export default AppBarPagename;
