96c0d800c11c4d4a740d790caf022f4018e2d717
chasura416 <chasura416@naver.com>
2023-08-10 19:03:18 +0900

    chore: PR

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,11 @@
 // import { Fnd, Cmp } from 'pds-3-14'; // For Testing NPM package
 import { Fnd, Cmp } from './lib/index'; // For Testing Local Files
-import ButtonFAB from './lib/components/ButtonFAB';
 
 function App() {
   return (
     <>
       <Fnd.FoundationGlobalStyles />
-      <Fnd.LayoutStyles>
-      {/* <ButtonFAB.Round>
-        + 문항 추가하기
-      </ButtonFAB.Round> */}
-      <ButtonFAB.Circle 
-      onClickEffect={() =>{
-        window.location.href = 'https://naver.com'
-      }}
-      />
-      </Fnd.LayoutStyles>
+      <Fnd.LayoutStyles></Fnd.LayoutStyles>
     </>
   );
 }
