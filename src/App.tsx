9bcc7de33a1d7720f6f32d2a2a0fed3ac5d1d54b
chasura416 <chasura416@naver.com>
2023-08-11 12:40:54 +0900

    chore: PR

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,12 @@
 import { useEffect, useState } from 'react';
 import { Fnd, Cmp } from './lib/index'; // For Testing Local Files
-import {CalendarStyle} from './lib/components/Picker';
-import Time from './lib/components/Picker/Time/index';
 
 function App() {
-  const [dates, setDates] = useState<[Date | null, Date | null] | null>([null, null]);
-  useEffect(() => {
-    console.log(dates);
-  }, [dates]);
+  
   return (
     <>
       <Fnd.FoundationGlobalStyles />
-      <Fnd.LayoutStyles>
-        {/* <CalendarStyle.CalendarWrapper>
-          <CalendarStyle.Calendar dates={dates} setDates={setDates} />
-          <CalendarStyle.ButtonWrapper>
-            <Cmp.Button.Contained state='normal' size='medium'>
-              {'취소'}
-            </Cmp.Button.Contained>
-            <Cmp.Button.Contained state='enabled' size='medium'>
-              {'완료'}
-            </Cmp.Button.Contained>
-          </CalendarStyle.ButtonWrapper>
-        </CalendarStyle.CalendarWrapper> */}
-        <Time />
-      </Fnd.LayoutStyles>
+      <Fnd.LayoutStyles></Fnd.LayoutStyles>
     </>
   );
 }
