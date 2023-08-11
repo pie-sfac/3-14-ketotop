368a90d4c4ccb83c2306227eb040a048f34a7257
chasura416 <chasura416@naver.com>
2023-08-11 12:48:27 +0900

    chore: PR3

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
+// import { Fnd, Cmp } from 'pds-3-14'; // For Testing NPM package
 import { Fnd, Cmp } from './lib/index'; // For Testing Local Files
 
 function App() {
-
   return (
     <>
       <Fnd.FoundationGlobalStyles />
